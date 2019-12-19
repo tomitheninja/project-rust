6deb32c186b826ad6992e23fda589c10790cfdb3
tomitheninja <suditomi@pm.me>
2019-12-19 19:53:20 +0100

    Solve 15th

---
+++ src/15_lattice_paths.rs
@@ -0,0 +1,19 @@
+// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+// How many such routes are there through a 20×20 grid?
+
+fn factorial (n: u16) -> f64 {
+    let mut result = 1.0;
+    for i in 1..=n {
+        result *= i as f64;
+    }
+    result
+}
+
+fn main () {
+    const N: u16 = 20;
+    let result = factorial(N + N)
+        / factorial(N)
+        / factorial(N);
+    println!("{}", result);
+}
