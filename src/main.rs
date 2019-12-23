1c752e3bf5d00e3840b301dba4cf8376cbba4985
tomitheninja <suditomi@pm.me>
2019-12-23 03:09:51 +0100

    Remove comments, since imports are new sorted by ABC rather than euler

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
 mod problem_10001st_prime;
+mod problem_amicable_numbers;
+mod problem_counting_sundays;
 mod problem_even_fibonacci_numbers;
 mod problem_highly_divisible_triangular_number;
 mod problem_largest_palindrome_product;
 mod problem_largest_prime_factor;
 mod problem_largest_product_in_grid;
 mod problem_largest_product_in_series;
+mod problem_lattice_paths;
+mod problem_longest_collatz_sequence;
+mod problem_maximum_path_sum_1;
+mod problem_maximum_path_sum_2;
 mod problem_multiples_of_3_and_5;
+mod problem_non_abundant_sums;
+mod problem_number_letter_counts;
 mod problem_smallest_multiple;
 mod problem_special_pythagorean_triplet;
 mod problem_sum_square_difference;
 mod problem_summation_of_primes;
-// Solved with JavaScript
-mod problem_lattice_paths;
-mod problem_longest_collatz_sequence;
-// Solver with JavaScript
-mod problem_counting_sundays;
-mod problem_maximum_path_sum_1;
-mod problem_number_letter_counts;
-// Solver with JavaScript
-mod problem_amicable_numbers;
-// Not solved yet
-mod problem_non_abundant_sums;
-
-mod problem_maximum_path_sum_2;
 
 mod lib;
 
