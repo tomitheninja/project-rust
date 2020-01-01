035953acd612549bfe769018a98443ca6c07e47f
tomitheninja <suditomi@pm.me>
2020-01-01 18:52:04 +0100

    :recycle: Refactor p003
    Move to /src/bin
    
    Hide result from the test

---
+++ src/bin/p003_largest_prime_factor.rs
@@ -0,0 +1,31 @@
+// Largest prime factor
+
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the number 600851475143 ?
+
+extern crate prime_factors;
+use prime_factors::PrimeFactor;
+
+fn compute(n: u64) -> u64 {
+    PrimeFactor::new(n).last().unwrap()
+}
+
+fn main() {
+    println!("p003: {}", compute(600851475143));
+}
+
+#[cfg(test)]
+mod p003_tests {
+    use super::compute;
+
+    #[test]
+    fn test_13195() {
+        assert_eq!(29, compute(13195));
+    }
+
+    #[test]
+    fn test_600851475143() {
+        assert_eq!(57, compute(600851475143) % 100);
+    }
+}
