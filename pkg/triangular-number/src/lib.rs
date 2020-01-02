cdf8f40028ccfb45d39e19e1dd7d84a4607f15f9
tomitheninja <suditomi@pm.me>
2020-01-02 01:22:59 +0100

    :sparkles: Implement TriangularNumber

--- pkg/triangular-number/src/lib.rs
+++ pkg/triangular-number/src/lib.rs
@@ -1,7 +1,52 @@
+#[derive(Clone)]
+pub struct TriangularNumber {
+    index: usize,
+    value: u64,
+}
+
+impl TriangularNumber {
+    pub fn new() -> TriangularNumber {
+        TriangularNumber { index: 0, value: 0 }
+    }
+
+    pub fn get_index(&self) -> usize {
+        self.index
+    }
+
+    pub fn get_value(&self) -> u64 {
+        self.value
+    }
+}
+
+impl Iterator for TriangularNumber {
+    type Item = TriangularNumber;
+    fn next(&mut self) -> Option<Self::Item> {
+        let prev_state = self.clone();
+        self.index += 1;
+        self.value += self.index as u64;
+        Some(prev_state)
+    }
+}
+
 #[cfg(test)]
-mod tests {
+mod tests_triangular_number {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn initial_index_is_zero() {
+        assert_eq!(0, TriangleNumber::new().get_index());
+    }
+    #[test]
+    fn initial_value_is_zero() {
+        assert_eq!(0, TriangleNumber::new().get_index());
+    }
+    #[test]
+    fn index_matches() {
+        let index = 10;
+        assert_eq!(index, TriangleNumber::new().nth(index).unwrap().get_index());
+    }
+    #[test]
+    fn test_third() {
+        assert_eq!(0 + 1 + 2 + 3, TriangleNumber::new().nth(3).unwrap().get_value());
     }
 }
