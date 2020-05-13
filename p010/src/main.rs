ded139ce94350f716dfdcd7827c0729eff0eb610
Südi Tamás <suditomi@pm.me>
2020-05-13 15:05:01 +0200

    🏁 Solve p010

---
+++ p010/src/main.rs
@@ -0,0 +1,34 @@
+//! The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+//!
+//! Find the sum of all the primes below two million.
+
+extern crate primes;
+
+fn main() {
+    println!("{}", solve(10));
+}
+
+/// Sum of primes below `n`
+fn solve(n: usize) -> u64 {
+    primes::sieve_of_eratosthenes(n)
+        .iter()
+        .zip(0..)
+        .filter(|(&is_prime, _)| is_prime)
+        .map(|(_, i)| i as u64)
+        .sum::<u64>()
+}
+
+#[cfg(test)]
+mod p010 {
+    use super::solve;
+
+    #[test]
+    fn solve_for_10() {
+        assert_eq!(17, solve(10));
+    }
+
+    #[test]
+    fn solve_for_two_million() {
+        assert_eq!(142913828922, solve(2_000_000))
+    }
+}
