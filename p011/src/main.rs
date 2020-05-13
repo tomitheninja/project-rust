5f12c3d41ab856e804fcdf4c9fdf324e958c8c4e
Südi Tamás <suditomi@pm.me>
2020-05-13 21:46:25 +0200

    📝 Write docs

--- p011/src/main.rs
+++ p011/src/main.rs
@@ -1,3 +1,7 @@
+//! In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+//!
+//! What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+
 macro_rules! data {
      ( $( $x:expr ) + ) => {
         {
--- p013/src/main.rs
+++ p013/src/main.rs
@@ -1,3 +1,5 @@
+//! Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
 extern crate num_bigint;
 
 use num_bigint::BigUint;
